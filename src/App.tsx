a8d3dc83f24796b202b26e18b4774eba8201b04f
Preston Jones <psmak4@gmail.com>
2023-11-07 18:14:33 -0500

    Update App imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import ActionBar from './components/actionBar'
-import Card from './components/card'
-import Navigation from './components/navigation'
-import Page from './components/page'
-import Pagination from './components/pagination'
-import Title from './components/title'
+import ActionBar from '~/components/actionBar'
+import Card from '~/components/card'
+import Navigation from '~/components/navigation'
+import Page from '~/components/page'
+import Pagination from '~/components/pagination'
+import Title from '~/components/title'
 
 const App = () => {
 	return (
