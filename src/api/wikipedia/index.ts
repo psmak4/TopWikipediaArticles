b9cc3ed11c664f0a5d53275efa3d841c3a32bf4e
Preston Jones <psmak4@gmail.com>
2023-11-07 18:13:46 -0500

    Move Article interface to models folder

--- src/api/wikipedia/index.ts
+++ src/api/wikipedia/index.ts
@@ -1,12 +1,4 @@
-/**
- * Represents an article with its properties.
- * @interface
- */
-interface Article {
-	article: string // The title of the article.
-	views: number // The number of views the article has.
-	rank: number // The rank of the article.
-}
+import Article from '~/models/article'
 
 /**
  * Represents a response item with data for a specific project, access type, and date.
---
+++ src/models/article.ts
@@ -0,0 +1,11 @@
+/**
+ * Represents an article with its properties.
+ * @interface
+ */
+interface Article {
+	article: string // The title of the article.
+	views: number // The number of views the article has.
+	rank: number // The rank of the article.
+}
+
+export default Article
