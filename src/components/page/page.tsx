c166d73cbafc22e73d194e3c8f8819e33352f418
Preston Jones <psmak4@gmail.com>
2023-11-06 22:45:43 -0500

    Add children prop to Page component

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -1,7 +1,8 @@
+import { PropsWithChildren } from 'react'
 import styles from './styles.module.css'
 
-const Page = () => {
-	return <div className={styles.page}></div>
+const Page = ({ children }: PropsWithChildren) => {
+	return <div className={styles.page}>{children}</div>
 }
 
 export default Page
