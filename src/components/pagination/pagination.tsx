9767def5b338a8c381c59dc2d8d43c8bd5263fbe
Preston Jones <psmak4@gmail.com>
2023-11-11 09:06:14 -0500

    Add Pagination implementation

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,7 +1,38 @@
+import { ReactNode, useEffect, useState } from 'react'
+import ArticlesStore, { setCurrentPage } from '~/stores/articlesStore'
 import styles from './styles.module.css'
 
 const Pagination = () => {
-	return <div className={styles.pagination}></div>
+	const { articles, currentPage, pageSize } = ArticlesStore()
+	const [pageCount, setPageCount] = useState<number>(0)
+
+	const getPageButtons = (): ReactNode[] => {
+		const start = currentPage > 2 ? currentPage - 2 : 1
+		const end = Math.max(currentPage + 1 < pageCount ? currentPage + 1 : pageCount, 4)
+		const buttons: ReactNode[] = []
+
+		for (let i = start; i <= end; i++) {
+			const style = i === currentPage ? `${styles.pageButton} ${styles.pageButtonSelected}` : `${styles.pageButton}`
+			buttons.push(
+				<button key={`pageButton-${i}`} className={style} onClick={() => setCurrentPage(i)}>
+					<span className={styles.buttonContent}>{i}</span>
+				</button>,
+			)
+		}
+
+		return buttons
+	}
+
+	useEffect(() => {
+		const count = Math.ceil(articles.length / pageSize)
+		setPageCount(count)
+	}, [articles, pageSize])
+
+	return (
+		<div className={styles.pagination}>
+			<div className={styles.buttonContainer}>{getPageButtons()}</div>
+		</div>
+	)
 }
 
 export default Pagination
