1db323db93cf9eac797dea7300ce8b2844a61324
Preston Jones <psmak4@gmail.com>
2023-11-12 20:14:56 -0500

    Setup testing with Vitest and Testing Library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
@@ -5,4 +7,10 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), viteTsconfigPaths()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/test/setup.ts',
+		css: false,
+	},
 })
