3b171cf167ec136e1b32d6726879eddf164c5801
Preston Jones <psmak4@gmail.com>
2023-11-13 21:41:34 -0500

    Remove Vitest and use Jest instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
@@ -7,10 +5,4 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), viteTsconfigPaths()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './src/test/setup.ts',
-		css: false,
-	},
 })
